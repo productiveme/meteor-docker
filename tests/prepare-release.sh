3c2bfd82925cce0bc7b1fd6d4edaa97477ffa5ba
Jaco Swarts <jaco@productive.me>
2022-09-23 06:16:44 +0200

    Change release for multiple architectures

--- tests/prepare-release.sh
+++ tests/prepare-release.sh
@@ -1,6 +1,6 @@
 set -e
 
-docker build -t zodern/meteor ./image
-docker build -t zodern/meteor:root ./root-image
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t productiveme/meteor ./image
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t productiveme/meteor:root ./root-image
 
 semantic-release --debug
